4a6dcde52cc24465c66345c291fb530ba7d33ee9
Sherzod <qudratov25@mail.ru>
2026-02-17 23:03:47 +0500

    fix excel import

--- index.js
+++ index.js
@@ -310,8 +310,8 @@ bot.action('admin_export', async (ctx) => {
 
     const buffer = await wb.xlsx.writeBuffer();
     return ctx.replyWithDocument(
-        { source: buffer, filename: 'users_with_videos.xlsx' },
-        { caption: "📊 Video Yuborganlar ro'yxati" }
+        { source: buffer, filename: 'xisobot.xlsx' },
+        { caption: "Video yuborganlar" }
     );
 });
 
